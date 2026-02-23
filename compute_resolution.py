27ce8381593f158ab9b313f2c45be7538921f2cf
cameronpoe <cameronmpoe@gmail.com>
2026-02-23 14:03:23 -0800

    updated for new FW

--- compute_resolution.py
+++ compute_resolution.py
@@ -9,6 +9,7 @@ FULL_FREQ = 5e9
 DECIMATION = 1
 FLIP_FREQ = 1.92e3
 FORMAT_DATA = 0
+CH1, CH2 = 0, 1
 
 def format_data(data_path):
     """Reformat muxed data from the RFSoC.
@@ -45,95 +46,84 @@ def format_data(data_path):
 
     return data 
 
-def get_iq_data(data_path):
+def get_iq_data(data, ch1, ch2):
 
-    try:
-        data = np.load(data_path)
-    except:
-        print('Error: cannot open numpy file.')
-        raise
+    num_samp = data.shape[1]
 
-    num_pairs = data.shape[0]
-    num_samp = data.shape[2]
-
-    iq_data = np.zeros((2, num_pairs, num_samp), dtype=np.complex128)
-    iq_data[0] = data[:,0,:] + 1j*data[:,1,:]
-    iq_data[1] = data[:,2,:] + 1j*data[:,3,:]
+    iq_data = np.zeros((2, num_samp), dtype=np.complex128)
+    iq_data[0] = data[2*ch1,:] + 1j*data[2*ch1 + 1,:]
+    iq_data[1] = data[2*ch2,:] + 1j*data[2*ch2 + 1,:]
     
-    return iq_data, num_pairs, num_samp
+    return iq_data, num_samp
 
-def process_to_dc(iq_data, num_pairs, num_samp, samp_freq):
+def process_to_dc(iq_data, num_samp, samp_freq):
 
+    # Get FFT data and freq domain and sort (fftfreq doesn't give freqs back in ascending order)
     freq_domain = fftfreq(num_samp, 1/samp_freq)
-    iq_data_freq = fft(blackman(num_samp)*iq_data, axis=2)
+    freq_sort = np.argsort(freq_domain)
+    freq_domain = freq_domain[freq_sort]
+    iq_data_freq = fft(blackman(num_samp)*iq_data, axis=1)[:,freq_sort]
 
-    # Finds index of the highest-power signal (carrier)
-    carrier_indices = np.argmax(np.abs(iq_data_freq), axis=2)
+    # Finds index of the highest-power signal (i.e. the carrier)
+    carrier_indices = np.argmax(np.abs(iq_data_freq), axis=1)
 
     # Slices of indices immediately (3 away) around carrier index
-    slices = carrier_indices[:,:,np.newaxis] + np.arange(-3, 4)[np.newaxis,np.newaxis,:]
+    slices = carrier_indices[:,np.newaxis] + np.arange(-3, 4)[np.newaxis,:]
 
     # Frequencies around carrier frequency
-    freq_neighborhoods = np.take_along_axis(np.repeat(np.repeat(freq_domain[np.newaxis,:], slices.shape[1], axis=0)[np.newaxis,:,:], slices.shape[0], axis=0), slices, axis=2) # gross repeating thing in first position just puts a copy of freq domain into every entry of a (2,500) array
-
+    freq_neighborhoods = np.take_along_axis(np.repeat(freq_domain[np.newaxis,:], slices.shape[0], axis=0), slices, axis=1)
+  
     # FFT spectrum around carrier frequency
-    iq_data_freq_neighborhoods = np.abs(np.take_along_axis(iq_data_freq, slices, axis=2))
+    iq_data_freq_neighborhoods = np.abs(np.take_along_axis(iq_data_freq, slices, axis=1))
     slices = None
 
     # Carrier frequency is found by weighted average of frequencies around the highest-power one. Since DC peak is not a delta, since if the true frequency is shifting, power is shifting among the FFT bins. 
-    carrier_freqs = np.sum(freq_neighborhoods * iq_data_freq_neighborhoods/np.sum(iq_data_freq_neighborhoods, axis=2)[:,:,np.newaxis], axis=2)
+    carrier_freqs = np.sum(freq_neighborhoods * iq_data_freq_neighborhoods/(np.sum(iq_data_freq_neighborhoods, axis=1)[:,np.newaxis]), axis=1)
     iq_data_freq_neighborhoods, freq_neighborhoods = None, None
 
     # Phases of the carrier signal
-    row_inds = np.arange(2)[:,None]
-    col_inds = np.arange(num_pairs)
-    carrier_phases = np.abs(np.angle(iq_data_freq[row_inds, col_inds, carrier_indices]))
+    carrier_phases = np.unwrap(np.angle(np.take_along_axis(iq_data_freq, np.array([carrier_indices]).T, axis=1)))
+    carrier_phases = carrier_phases.T[0]
 
     # Down-mixes and eliminates any the phase due to the carrier
-    iq_data = iq_data * np.exp(-1j * (2*np.pi*carrier_freqs[:,:,np.newaxis]/samp_freq*np.arange(num_samp) + carrier_phases[:,:,np.newaxis]))
+    iq_data = iq_data * np.exp(-1j * (2*np.pi*carrier_freqs[:,np.newaxis]/samp_freq*np.arange(num_samp) + carrier_phases[:,np.newaxis]))
 
     # Takes away any remnant phases between I/Q data
-    avg_phases = np.average(np.unwrap(np.angle(iq_data)), axis=2) # unwrap is very important here b/c we're averaging. if angle is fluctuating around +/- pi, the average of np.angle() could be ~0, but average of np.unwrap(np.angle()) will be the correct phase
-    iq_data *= np.exp(-1j*avg_phases[:,:,np.newaxis])
+    avg_phases = np.average(np.unwrap(np.angle(iq_data)), axis=1) # unwrap is very important here b/c we're averaging. if angle is fluctuating around +/- pi, the average of np.angle() could be ~0, but average of np.unwrap(np.angle()) will be the correct phase
+    iq_data *= np.exp(-1j*avg_phases[:,np.newaxis])
     return np.real(iq_data)    
 
 def compute_ddf(dc_data, num_samp, samp_freq):
 
-    time_domain = np.arange(num_samp)/samp_freq
-    mask_l = (time_domain >= 0) & (time_domain < 1/FLIP_FREQ)
-    mask_r = (time_domain >= 1/FLIP_FREQ) & (time_domain < 2/FLIP_FREQ)
+    # We want to trim the DC data so that only an integer number of helicity window pairs can fit inside it
+    num_samples_per_helicity_window = int(np.round(samp_freq / FLIP_FREQ))
+    num_samples_per_window_pair = num_samples_per_helicity_window * 2
+    extra_samples = dc_data.shape[-1]%num_samples_per_window_pair
+    dc_data = dc_data[:,:-extra_samples]
+    num_windows = dc_data.shape[1] // num_samples_per_window_pair
 
-    window_length = mask_l.sum() + mask_r.sum()
-    num_windows = dc_data.shape[-1]//window_length
+    dc_data = dc_data.reshape((-1,num_windows,num_samples_per_window_pair))
 
-    rdfs_full = np.zeros((num_windows, 2, dc_data.shape[1]), dtype=np.float64)
-    ddfs_full = np.zeros((num_windows, dc_data.shape[1]), dtype=np.float64)
+    ldata = np.mean(dc_data[:,:,:num_samples_per_helicity_window], axis=2)
+    rdata = np.mean(dc_data[:,:,num_samples_per_helicity_window:], axis=2)
 
-    for i in range(num_windows):
-        
-        mask_l = np.roll(mask_l, i*window_length)
-        mask_r = np.roll(mask_r, i*window_length)
+    rdfs = (rdata - ldata)/(rdata+ldata)
+    ddfs = rdfs[0,:] - rdfs[1,:]
 
-        l_data = np.average(dc_data[:,:,mask_l], axis=2)
-        r_data = np.average(dc_data[:,:,mask_r], axis=2)
-
-        rdfs = (r_data - l_data)/(r_data + l_data)
-        ddfs = 1/np.sqrt(2) * (rdfs[0] - rdfs[1])
-
-        rdfs_full[i,:,:] = np.copy(rdfs)
-        ddfs_full[i,:] = np.copy(ddfs)
-
-    return ddfs_full, rdfs_full
+    return ddfs, rdfs
 
 if __name__ == '__main__':
 
     args = sys.argv
 
+
     for i, arg in enumerate(args):
         if arg == '-d' or arg == '--dec':
             DECIMATION = int(args[i+1])
-        if arg == '-f' or arg == '--format':
+        elif arg == '-f' or arg == '--format':
             FORMAT_DATA = 1
+        elif arg == '-chs' or arg == '--channels':
+            CH1, CH2 = int(args[i+1]), int(args[i+2])
         elif arg == '-h' or arg == '--help':
             print('''Usage: python3 compute_resolution.py <npy_file_path> [-h | --help] [-options]
 
@@ -145,6 +135,12 @@ OPTIONS
 
     [-d <dec> | --dec <dec>]
         Sets decimation to the integer <dec>. Sampling frequency is then 5e9/<dec>. Default is <dec>=1.
+                  
+    [-f | --format ]
+        Formats data from muxed RFSoC format into 2D numpy array indexed by I/Q channel.
+    
+    [-chs <ch1> <ch2> | --channels <ch1> <ch2> ]
+        Chooses which two channels to compare to compute the resolution.
 ''')
             exit()
     
@@ -163,14 +159,27 @@ OPTIONS
 
     print(f'Data file path: {args[1]}')
     print(f'tmp/ directory path: {tmp_dir_path}')
-    print(f'Sampling frequency: {round(SAMP_FREQ*1e-6, 3)} MHz')    
+    print(f'Sampling frequency: {round(SAMP_FREQ*1e-6, 3)} MHz')   
+    print(f'Channel 1: {CH1}') 
+    print(f'Channel 2: {CH2}')
     
     data_path = args[1]
+
     if FORMAT_DATA:
-        format_data(data_path)
-        exit()
-    iq_data, num_pairs, num_samp = get_iq_data(data_path)
-    dc_data = process_to_dc(iq_data, num_pairs, num_samp, SAMP_FREQ)
+        data = format_data(data_path)
+    else:
+        try:
+            data = np.load(data_path)
+        except:
+            print(f'Error: cannot open file: {data_path}')
+            raise
+
+
+    iq_data, num_samp = get_iq_data(data, CH1, CH2)
+
+    print(f'Number of samples: {num_samp}')
+
+    dc_data = process_to_dc(iq_data, num_samp, SAMP_FREQ)
     ddfs, rdfs = compute_ddf(dc_data, num_samp, SAMP_FREQ)
 
     np.savez(tmp_dir_path + 'ddfs_rdfs', ddfs=ddfs, rdfs=rdfs)
